5b08c4c8d69ccaa5736c11e52cde79e42d00e27c
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-08-09 17:33:04 -0300

    ejemplos

---
+++ ejemploprueba1/ejemploprueba1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n;
+int j;
+int resultado;
+Console.WriteLine("ingrese un numero");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine("Igrese otro");
+j = int.Parse(Console.ReadLine());
+resultado = n + j;
+Console.WriteLine("el resultado es: " + resultado);
+
+
